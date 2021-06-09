91e42c9c3a1ab9ea46fcff5c37b2651a55077f88
Vahagn Sargsyan <vahagnsargsyan@courseloop.com>
2021-06-09 11:12:13 +1000

    Header information added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import AboutMe from './components/AboutMe'
+
+const Root = styled('div')`
+  justify-content: center;
+  text-align: center;
+  background: lightblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+`
 
 function App() {
   return (
-    <div className="App">
-      <p>Vahagn Sargsyan</p>
-      <p>I am novice Front End developer.</p>
-    </div>
+    <Root>
+      <AboutMe/>
+    </Root>
   );
 }
 
---
+++ src/components/AboutMe.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text } from './Text';
+
+const Header = styled('div')`
+  position: absolute;
+  width: 60vw;
+  padding: 10px;
+  display: flex;
+`;
+
+const Container = styled('div')`
+  align-items: center;
+  padding: 10px;
+`
+
+const LeftSide = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  min-width: 30%;
+`
+
+const RightSide = styled(Container)`
+  flex-grow: 4;
+  align-self: center;
+`
+
+const Headshot = styled('img')`
+  max-height:190%;  
+  background: red;
+  transform: translate(-10%, -10%);
+`;
+
+const Title = styled('h1')`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  align-self: center;
+`;
+
+const HeadContainer = styled('div')`
+  height: 100px;
+  width: 100px;
+  overflow: hidden;
+  border-radius: 50%;
+  border: solid yellow;
+  flex-grow: 4
+`
+
+
+
+function AboutMe() {
+
+  return (
+    <Header>
+      <LeftSide>
+      <HeadContainer>
+        <Headshot src={"/Headshot.jpg"}/>
+      </HeadContainer>
+        <Title>Vahagn Sargsyan</Title>
+      </LeftSide>
+      <RightSide>
+        <Text>I am novice FrontEnd developer with many years of custom ServiceNow BackEnd development. I have many hobbies outside of development which include, but are not limited to Golf 🏌🏼‍♂️, Functional Training 🏋🏼‍♀️ and more Golf 🏌🏼‍♂️</Text>
+      </RightSide>
+    </Header>
+  );
+}
+
+export default AboutMe;
